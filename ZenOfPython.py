fcf330df240100864a28f1e15ed21afd161d8736
Emiel 't Lam <99067718@mydavinci.nl>
2022-04-21 11:11:24 +0200

    used with statement

--- ZenOfPython.py
+++ ZenOfPython.py
@@ -1,6 +1,17 @@
 import time
-file = open('C:/Users/HP/Documents/Klas 1/SoftwareDevelopment/2022/Read-Write/read-files/README.md','r')
-lineList = file.readlines()
-for i in range(len(lineList)):
-    time.sleep(1)
-    print(lineList[i])
+
+with open("C:/Users/HP/Documents/Klas 1/SoftwareDevelopment/2022/Read-Write/read-files/README.md") as file:
+    content = file.readlines()
+    for i in range(len(content)):
+        time.sleep(1)
+        print(content[i])
+
+
+
+# oude code
+
+# file = open('C:/Users/HP/Documents/Klas 1/SoftwareDevelopment/2022/Read-Write/read-files/README.md','r')
+# lineList = file.readlines()
+# for i in range(len(lineList)):
+#     time.sleep(1)
+#     print(lineList[i])
